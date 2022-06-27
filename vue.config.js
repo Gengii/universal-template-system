4d10ca09cfe2970125d9f92d413e198166aaf77f
gengh <210565854@qq.com>
2022-06-27 16:49:35 +0800

    feat(vue.config.js): 解决跨域

--- vue.config.js
+++ vue.config.js
@@ -5,13 +5,12 @@ function resolve(dir) {
 }
 
 module.exports = {
-  chainWebpack: config => {
-    config.module.rules.delete("svg"); // 重点：删除默认配置中处理svg,
+  chainWebpack: (config) => {
+    config.module.rules.delete('svg') // 重点：删除默认配置中处理svg,
     config.module
       .rule('svg-sprite-loader')
       .test(/\.svg$/)
-      .include
-      .add(resolve('src/assets/imgs')) // 处理svg目录
+      .include.add(resolve('src/assets/imgs')) // 处理svg目录
       .end()
       .use('svg-sprite-loader')
       .loader('svg-sprite-loader')
@@ -19,4 +18,23 @@ module.exports = {
         symbolId: 'icon-[name]'
       })
   },
-};
+  transpileDependencies: true,// 新创建vue项目后,会出现第一行代码爆红的现象,解决它的方法: transpileDependencies: true
+  publicPath: './',
+  devServer: {
+    open: true,
+    port: 9527, // 端口号码
+    https: false,
+    host: 'localhost', // 主机名
+    proxy: {
+      // 匹配 '/dev-api' 开头的请求
+      [process.env.VUE_APP_BASE_API]: {
+        target: process.env.VUE_APP_SERVICE_URL,
+        changeOrigin: true, // 开启代理
+        pathRewrite: {
+          ['^' + process.env.VUE_APP_BASE_API]: ''
+        }
+      }
+    }
+  },
+  lintOnSave: true // 设置是否在开发环境下每次保存代码时都启用 eslint验证
+}
